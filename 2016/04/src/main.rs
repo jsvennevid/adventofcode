674a5c8ddea9c5cfac428d0d809ed4454f8b7021
Jesper Svennevid <jsvennevid@users.noreply.github.com>
2016-12-19 20:31:04 +0100

    Added day 4 solution

--- 2016/04/src/main.rs
+++ 2016/04/src/main.rs
@@ -13,22 +13,48 @@ use std::io;
 
 struct Room {
     name: String,
+    raw_name: String,
     id: i32,
     checksum: String
 }
 
 impl Room {
-    fn new(name: &str, id: i32, checksum: &str) -> Room {
-        Room {name: name.to_string(), id: id, checksum: checksum.to_string()}
+    fn new(name: &str, raw_name: &str, id: i32, checksum: &str) -> Room {
+        Room {name: name.to_string(), raw_name: raw_name.to_string(), id: id, checksum: checksum.to_string()}
     }
 
     fn valid(&self) -> bool {
-        let groups = self.name.chars().into_iter().group_by(|&x| x);
-        for group in groups {
-            println!("{}", group.sum::<i32>().abs());
-        }
+        let c1 = self.name.chars()
+            .sorted();
+        let groups = c1
+            .iter()
+            .group_by(|&x| x);
+        let chars = groups
+            .into_iter()
+            .map(|(c,n)| (-(n.count() as i32), c))
+            .sorted();
+        let c3 = chars.iter()
+            .map(|&(_,c)|c)
+            .cloned()
+            .collect::<String>();
+        let c4 = &c3[0..5];
 
-        self.name.len() > 0
+        self.checksum == c4
+    }
+
+    fn decrypt(&self) -> String {
+        let start = 'a' as i32;
+        let stop = 'z' as i32;
+        let c1 = self.raw_name.chars()
+            .map(|c| {
+                match c {
+                    '-' => { ' ' }
+                    c => ((start + (((c as i32)-start) + self.id) % (1 + (stop-start))) as u8) as char
+                }
+            })
+            .collect::<String>();
+
+        c1
     }
 }
 
@@ -42,10 +68,15 @@ impl Parser {
                     .chars()
                     .filter(|x| x != &'-')
                     .collect::<String>();
+                let raw_name = c
+                    .at(1)
+                    .unwrap()
+                    .chars()
+                    .collect::<String>();
                 let id = c.at(2).unwrap().parse::<i32>().unwrap();
                 let checksum = c.at(3).unwrap();
 
-                Some(Room::new(&name, id, checksum))
+                Some(Room::new(&name, &raw_name, id, checksum))
             }
             None => None
         }
@@ -72,7 +103,12 @@ impl Parser {
 fn main() {
     let rooms = Parser::load("input.txt").unwrap_or(Vec::new());
 
-    for i in rooms {
-        println!("room: {} {} {} {}", i.name, i.id, i.checksum, i.valid());
+    let sum = rooms.iter().filter(|&r| r.valid()).map(|ref r| r.id).sum::<i32>();
+
+    println!("SUM {}", sum);
+
+    match rooms.iter().find(|&r| r.valid() && r.decrypt() == "northpole object storage") {
+        Some(room) => { println!("ROOM {}", room.id) }
+        None => println!("NO ROOM")
     }
 }
